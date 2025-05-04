130bb3eea31e3d75a90b4d58e4fd489b3a98ab56
Tatsuya_M2667 <152606971+TatsuyaM2667@users.noreply.github.com>
2025-05-04 22:42:19 +0900

    Update CanSat_GoogleSheet_ver1.0.ino

--- CanSat_GoogleSheet_ver1.0.ino
+++ CanSat_GoogleSheet_ver1.0.ino
@@ -1,14 +1,3 @@
-/*
-  Rui Santos
-  Complete project details at https://RandomNerdTutorials.com/esp32-datalogging-google-sheets/
-
-  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files.
-  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-  Adapted from the examples of the Library Google Sheet Client Library for Arduino devices: https://github.com/mobizt/ESP-Google-Sheet-Client
-
-google spreadsheetの管理権をサービスアカウントに付与し、データを代入。
-*/
-
 #include <Arduino.h>
 #include <WiFi.h>
 #include "time.h"
